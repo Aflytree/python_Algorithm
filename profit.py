8b2a4bb427f04c6a4fbc8694ae441da14c63074d
afly <zhangaifei.2008@163.com>
2019-06-09 21:44:39 +0800

    初始化，查找算法

---
+++ profit.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#================================================================
+#   Copyright (C) 2019 Lynxi Ltd. All rights reserved.
+#   
+#   @name          ：profit.py
+#   @author        ：Afly
+#   @date          ：2019.06.09
+#   @description   ：
+#
+#================================================================
+import sys
+
+S = [10,4,8,7,9,6,2,5,3]
+maxprofit = 0
+buyday = 0
+sellday = 0
+
+#最大获利
+for i in range(len(S) - 1):
+    print(i)
+    for j in range(i+1, len(S)):
+        if S[j] - S[i] > maxprofit:
+            maxprofit = S[j] - S[i]
+            buyday = i
+            sellday = j
+
+print("b{0},s{1},maxprofit{2}".format(buyday, sellday,maxprofit))
+"""
+    判断S中是否有i,j，使得M = S[i] + S[j]
+    输入：list,以及 M - S[i]
+    返回：
+"""
+
+def binaryFind(S, m):
+    if len(S) == 0:
+        return -1
+    i = int(len(S) / 2)
+    if S[i] == m:
+        return i
+    if S[i] > m and i - 1 >= 0:
+        return binaryFind(S[0:i-1], m)
+    if S[i] < m and i+1 < len(S):
+        return binaryFind(S[i: len(S)], m)
+
+    return  -1
+
+#将S升序排列
+S.sort()
+print(S)
+M = 9
+success = False
+for i in range(len(S)):
+    m = M - S[i]
+    j = binaryFind(S,m)
+    if j != -1 and j != i:
+        print("存在i和j使得S[{0}] + S[{1}] = {2}".format(i, j, M))
+        success = True
+        break
+if success != True:
+    print("不存在i和j使得S[I] + S[J] = {0}".format(M))
+
+
+
+
+
